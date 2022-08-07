ab93b07bd772769bd00ca6243394d53e246ca5b9
jmejiamu <jmejiamu@gmail.com>
2022-08-07 14:58:39 -0700

    update(background):added background colors

---
+++ pokedex/src/constant/themes/backgroundType.ts
@@ -0,0 +1,16 @@
+export const backgrounds = {
+  background_type_bug: '#8BD674',
+  background_type_dark: '#6F6E78',
+  background_type_electric: '#F2CB55',
+  background_type_fairy: '#EBA8C3',
+  background_type_fighting: '#EB4971',
+  background_type_fire: '#FFA756',
+  background_type_ground: '#F78551',
+  background_type_ice: '#91D8DF',
+  background_type_normal: '#B5B9C4',
+  background_type_poison: '#9F6E97',
+  background_type_psychic: '#FF6568',
+  background_type_rock: '#D4C294',
+  background_type_steel: '#4C91B2',
+  background_type_water: '#58ABF6',
+};
--- pokedex/src/index.tsx
+++ pokedex/src/index.tsx
@@ -1,12 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {colors} from './constant/themes';
 
 const Root = () => {
   return (
     <View>
-      <Text>Root</Text>
+      <Text style={styles.font}>Root</Text>
     </View>
   );
 };
 
 export default Root;
+
+const styles = StyleSheet.create({
+  font: {
+    color: colors.type_electric,
+  },
+});
